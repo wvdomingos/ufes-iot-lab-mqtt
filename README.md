4a42625002c17726f19850d061dbd70b3966402a
wvdomingos <wandervilhalvadomingos@gmail.com>
2021-08-04 21:21:33 -0300

    Add arquivos python

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ufes-iot-lab-mqtt
-Laboratório para testar o Protocolo MQTT
+# Laboratório MQTT
+### Disciplina: Internet das Coisas
+### Prof. Dr. quiVinícius Fernandes Soares Mota
+
+```
+Descrição do projeto: Laboratório para testar o Protocolo MQTT
+```
+
+# Instalação
+$ pip install paho-mqtt
+
+$ sudo apt-get install mosquitto mosquitto.clients
+
---
+++ mqtt_sub.py
@@ -0,0 +1,24 @@
+# Um simples exemplo do MQTT Subscribe
+
+import paho.mqtt.client as paho
+import sys
+
+def onMessage(client, userdata, msg):
+    print(msg.topic + ": " + msg.payload.decode())
+
+client = paho.Client()
+client.on_message = onMessage()
+
+if client.connect("localhost", 1883, 60) != 0:
+    print("Could not connect to MQTT Broker!")
+    sys.exit(-1)
+
+client.subscribe("test/status")
+
+try:
+    print("Press CTRL+C to exit...")
+    client.loop_forever()
+except:
+    print("Disconnecting from broker")
+
+client.disconnect()
